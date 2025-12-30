c177dbc257e82c81fbc1c56b0104ee2fe17cf82b
dwainXDL <personal.dwain@gmail.com>
2025-12-30 23:52:27 +0530

    Fixed router bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import ContactAdmin from './pages/ContactAdmin'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/contact-admin" element={<ContactAdmin />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
